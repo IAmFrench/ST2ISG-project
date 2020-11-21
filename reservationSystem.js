f4c82517917208fadbc2ddf744a46dfb3e0618dc
Alexandre Parès <IAmFrench@users.noreply.github.com>
2020-11-21 09:38:06 +0000

    Add hotels room date reservations in the hotels object and start to implement the filter hotels by dates

--- reservationSystem.js
+++ reservationSystem.js
@@ -15,19 +15,19 @@ class reservationSystem {
     const hotelsObj = yaml.safeLoad(fs.readFileSync('./database/hotels.yaml', 'utf8'));
     const reservationsObj = yaml.safeLoad(fs.readFileSync('./database/reservations.yaml', 'utf8'));
 
-    const hotelsListKeys = Object.keys(hotelsObj.hotels);
+    const hotelsListKeys = Object.keys(hotelsObj.hotels)
     for (const hotelName of hotelsListKeys) {
-      this.hotels.push(new hotel(hotelName, hotelsObj.hotels[hotelName].rooms));
+      this.hotels.push(new hotel(hotelName, hotelsObj.hotels[hotelName].rooms))
     }
 
-    const reservationsListKeys = Object.keys(reservationsObj.reservations);
+    const reservationsListKeys = Object.keys(reservationsObj.reservations)
     for (const reservationId of reservationsListKeys) {
-      const startDate = moment(reservationsObj.reservations[reservationId].startDate);
-      const startDateString = startDate.format('YYYY-MM-DD');
-      const endDate = moment(reservationsObj.reservations[reservationId].endDate);
-      const duration = moment.duration(endDate.diff(startDate)).as('days');
-      const hotelName = reservationsObj.reservations[reservationId].hotel;
-      const rooms = reservationsObj.reservations[reservationId].rooms;
+      const startDate = moment(reservationsObj.reservations[reservationId].startDate)
+      const startDateString = startDate.format('YYYY-MM-DD')
+      const endDate = moment(reservationsObj.reservations[reservationId].endDate)
+      const duration = moment.duration(endDate.diff(startDate)).as('days')
+      const hotelName = reservationsObj.reservations[reservationId].hotel
+      const rooms = reservationsObj.reservations[reservationId].rooms
 
       this.reservations.push(new reservation(reservationId, startDateString, duration, hotelName, rooms));
     }
@@ -43,25 +43,38 @@ class reservationSystem {
       console.log("Number of rooms in this hotel " + hotel.rooms.length)
       if (hotel.rooms.length < roomsRequired) {
         // We remove this particular hotel from the list of return hotels
+        console.log('This hotel have less room than required for this reservation')
         delete resulthotels[indexH]
       }
     })
-
     // Reconstruct the array (trim undefinded)
     resulthotels = resulthotels.filter(function (el) {
       return el != null
     })
 
 
-    // TODO FROM HERE
+    // Search by date from hotels rooms for eachroom if it's available at the required dates
+    // Find rooms that fit this constraint
+    let dates = []
+    for (let index = 0; index < duration; index++) {
+      dates.push(moment(startDate).add(index, 'days').format('YYYY-MM-DD'))      
+    }
+    resulthotels.forEach((hotel, indexH) => {
+      const hotelRoomsListKeys = Object.keys(hotel.rooms)
+      for (const roomNumber of hotelRoomsListKeys) {
+        if (hotel.rooms[roomNumber] != null) {
+          // dates console.log()
+          const roomDatesKeys = Object.keys(hotel.rooms[roomNumber].reservations)
+          for (const reservationDate of roomDatesKeys) {
+            console.log("r: " + moment(reservationDate).format('YYYY-MM-DD'))
+          }
+        }
+        
 
-
-    this.hotels.forEach((hotel, indexH) => {
-      // 
-      hotel.rooms.forEach((room, indexR) => {
-        console.log(indexR + ": " + room)
-      })
+      }
     })
+    // Build the response, return true/false, if true add the list of potentia hotels + rooms 
+
   }
 }
 exports.reservationSystem = reservationSystem;
